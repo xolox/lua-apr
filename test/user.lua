1db976e84c884915510ea3bb4736d7acd6a0e016
Peter Odding <peter@peterodding.com>
2011-02-23 09:28:49 +0100

    Bug fix for test/user.lua: Don't rely on $HOME
    Because it might have been changed temporarily and this doesn't
    necessarily mean there's anything wrong with apr.user_homepath_get().
    Thanks to Enrico Tassi for the heads up!

--- test/user.lua
+++ test/user.lua
@@ -3,13 +3,14 @@
  Unit tests for the user/group identification module of the Lua/APR binding.
 
  Author: Peter Odding <peter@peterodding.com>
- Last Change: February 11, 2011
+ Last Change: February 23, 2011
  Homepage: http://peterodding.com/code/lua/apr/
  License: MIT
 
 --]]
 
 local apr = require 'apr'
+local helpers = require 'apr.test.helpers'
 
 -- First check whether apr.user_get() works or returns an error.
 assert(apr.user_get())
@@ -31,5 +32,8 @@ end
 local env_home = apr.env_get 'HOME'
 if env_home then
   -- Match result of apr.user_homepath_get() against $HOME.
-  assert(apr.user_homepath_get(user) == env_home)
+  local apr_home = assert(apr.user_homepath_get(user))
+  if apr_home ~= env_home then
+    helpers.warning("$HOME == %q while apr.user_homepath_get(%q) == %q (you probably changed $HOME?)\n", env_home, user, apr_home)
+  end
 end
